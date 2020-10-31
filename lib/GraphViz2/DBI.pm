4b2c899f5682e04e69882c268de148e368fbf68a
Ed J <mohawk2@users.noreply.github.com>
2020-10-31 17:02:49 +0000

    2.51

--- lib/GraphViz2/DBI.pm
+++ lib/GraphViz2/DBI.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use warnings  qw(FATAL utf8); # Fatalize encoding glitches.
 
-our $VERSION = '2.50';
+our $VERSION = '2.51';
 
 use DBIx::Admin::TableInfo;
 use GraphViz2;
